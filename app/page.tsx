21993143220d04493f2be6926848590adc172fcc
Tee Parham <parhameter@gmail.com>
2024-01-12 18:43:28 -0700

    Add page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,264 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const interFont = Inter({ subsets: ["latin"] });
+
+const urls = {
+  billions:
+    "https://handbook.neighborland.com/measuring-what-matters-b635f20370b8",
+  faves:
+    "https://www.businesswire.com/news/home/20230718746480/en/Nextdoor-Announces-the-2023-Neighborhood-Faves-Winners-and-Unveils-Special-Bell-Ringing-Event-at-the-New-York-Stock-Exchange",
+  acquired:
+    "https://handbook.neighborland.com/nextdoor-acquires-neighborland-e12237b7e8fd",
+  rogues:
+    "https://devchat.tv/ruby-rogues/rr-356-geospatial-programming-in-ruby-with-daniel-azuma-and-tee-perham",
+};
+
+const Arrow = () => {
+  return (
+    <span
+      className={`inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none ${interFont.className}`}
+    >
+      -&gt;
+    </span>
+  );
+};
+
+interface LinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const Link = ({ href, children }: LinkProps) => {
+  return (
+    <a
+      href={href}
+      className="underline text-blue-700 dark:text-blue-300 underline-offset-2 hover:decoration-2"
+    >
+      {children}
+    </a>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="container flex min-h-screen flex-col items-center justify-between py-8">
+      <div className="">
+        <div className="mb-8 text-left">
+          <h1 className="text-opacity-90 dark:text-yellow-400">Tee Parham</h1>
+          <h2 className="text-opacity-70 dark:text-blue-400">
+            Idealist, code writer, technical leader.
+          </h2>
+        </div>
+        <div className="text-left leading-relaxed">
+          <section className="mb-8">
+            <h3 className="text-blue-400 dark:text-yellow-200">Work</h3>
+            <ul>
+              <li className="mb-3">
+                I was the technical lead for the Public Agencies Pillar and the
+                Organizations Admin team at{" "}
+                <Link href="https://nextdoor.com/">Nextdoor</Link> where I led
+                projects like <Link href={urls.faves}>Neighborhood Faves</Link>.
+              </li>
+              <li className="mb-3">
+                <Link href="https://neighborland.com/">Neighborland</Link> was a
+                public engagement platform designed for government agencies,
+                place-makers, and civic organizations. Our mission was to
+                empower residents to shape the development of their
+                neighborhoods. To do this, we helped improve the way civic
+                organizations collaborated with their communities. Over 3
+                million people participated on Neighborland, and our partners'
+                projects have yielded <Link href={urls.billions}>billions</Link>{" "}
+                of dollars in social and economic impact. Neighborland was{" "}
+                <Link href={urls.acquired}>acquired</Link> by Nextdoor in 2019.
+              </li>
+              <li className="mb-3">
+                I helped{" "}
+                <Link href="https://designerfund.com/">Designer Fund</Link>{" "}
+                attract and connect the world's best designers with top
+                technology companies by rebuilding the front end of their events
+                app.
+              </li>
+              <li className="mb-3">
+                I helped{" "}
+                <Link href="https://brightfunds.org/">Bright Funds</Link> build
+                a platform for charitable giving and volunteering.
+              </li>
+              <li className="mb-3">
+                I led development of an ERP system for{" "}
+                <Link href="https://www.nesterhosiery.com/">
+                  Nester Hosiery
+                </Link>{" "}
+                so they could make the world's best wool socks more efficiently.
+                My team helped the company manage manufacturing of over 5000
+                items and grow 20% annually for over 8 years. You can{" "}
+                <Link href="https://www.farmtofeet.com/">buy some socks</Link>{" "}
+                from their Farm to Feet brand.
+              </li>
+            </ul>
+          </section>
+          <section className="mb-8">
+            <h3 className="text-blue-400 dark:text-yellow-200">Internet</h3>
+            <div className="text-4xl dark:text-yellow-400">
+              <a
+                className="icon-github mr-3"
+                href="https://github.com/teeparham/"
+              ></a>
+              <a
+                className="icon-linkedin mr-3"
+                href="https://www.linkedin.com/in/teeparham"
+              ></a>
+              <a
+                className="icon-twitter mr-3"
+                href="https://twitter.com/teeparham"
+              ></a>
+              <a
+                className="icon-stackoverflow"
+                href="https://stackoverflow.com/users/162934/tee"
+              ></a>
+            </div>
+          </section>
+          <section className="mb-8">
+            <h3 className="text-blue-400 dark:text-yellow-200">Open source projects in ruby</h3>
+            <ul>
+              <li className="mb-3">
+                <Link href="https://github.com/teeparham/gemdiff">gemdiff</Link>{" "}
+                is a command-line tool to find source code for ruby gems. You
+                can compare source code differences between the current version
+                of a gem in your bundle and the latest version of the gem.
+                gemdiff connects gem version management (rubygems + bundler)
+                with source code (GitHub).
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/rgeo/rgeo">rgeo</Link> is a
+                geospatial data library for ruby. It wraps the GEOS and Proj C
+                libraries and provides spatial types such as point, linestring,
+                and polygon.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/rgeo/activerecord-postgis-adapter">
+                  activerecord-postgis-adapter
+                </Link>{" "}
+                is the PostGIS ActiveRecord adapter. Use it to connect
+                ActiveRecord models to your PostgreSQL + PostGIS database, with
+                all the geometry type methods from rgeo.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/haml/haml">Haml</Link> is a
+                templating engine for HTML. It's designed to make it both easier
+                and more pleasant to write HTML documents, by eliminating
+                redundancy, reflecting the underlying structure that the
+                document represents, and providing an elegant syntax that's both
+                powerful and easy to understand.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/neighborland/cache_rocket">
+                  cache_rocket
+                </Link>{" "}
+                improves fragment caching efficiency in Rails. CacheRocket
+                allows caching more generic html fragments and allowing the
+                contents of the cached fragments to be replaced with dynamic
+                content.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/neighborland/pres">pres</Link>{" "}
+                provides simple Rails presenter modules and classes.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/neighborland/sluggi">
+                  sluggi
+                </Link>{" "}
+                is a friendly_id-inspired slugging library for ActiveRecord
+                models. Use it to build nice-looking URLs. It provides basic
+                slugs, slug history, and the ability to define multiple slug
+                candidates.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/neighborland/scopy">scopy</Link>{" "}
+                provides common ActiveRecord model scopes for common columns
+                including created_at and name.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/neighborland/resque_solo">
+                  resque_solo
+                </Link>{" "}
+                ensures unique jobs in Resque.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/neighborland/anchored">
+                  anchored
+                </Link>{" "}
+                auto-links URLs in HTML text using ruby.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/rgeo/rgeo-geojson">
+                  rgeo-geojson
+                </Link>{" "}
+                provides GeoJSON serialization support to RGeo objects.
+              </li>
+              <li className="mb-3">
+                <Link href="https://github.com/rgeo/rgeo-activerecord">
+                  rgeo-activerecord
+                </Link>{" "}
+                provides RGeo ActiveRecord extensions, used by the PostGIS,
+                MySql, and Spatialite database adapters.
+              </li>
+            </ul>
+          </section>
+          <section className="mb-8">
+            <h3 className="text-blue-400 dark:text-yellow-200">Presentations</h3>
+            <ul>
+              <li className="mb-3">
+                I was a guest on Devchat.tv's{" "}
+                <Link href={urls.rogues}>Ruby Rogues podcast</Link> along with
+                RGeo creator Daniel Azuma. We discussed geospatial programming
+                in ruby.
+              </li>
+              <li className="mb-3">
+                I presented at Boulder Ruby about{" "}
+                <Link href="https://www.slideshare.net/teeparham/rails-html-fragment-caching-with-cache-rocket">
+                  HTML fragment caching in rails.
+                </Link>
+              </li>
+              <li className="mb-3">
+                I also presented at Boulder Ruby about{" "}
+                <Link href="https://www.slideshare.net/teeparham/gemdiff">
+                  gem dependency updates with gemdiff.
+                </Link>
+              </li>
+            </ul>
+          </section>
+          <section className="mb-8">
+            <h3>Be a winner</h3>
+            <ul>
+              <li>mastermind</li>
+            </ul>
+          </section>
+          <section className="mb-8">
+            <h3>See the possibilities</h3>
+            <ul>
+              <li>n-queens</li>
+            </ul>
+          </section>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center md:max-w-5xl md:w-full md:mb-0 md:grid-cols-4 md:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <h2 className="mb-3">
+            Link&nbsp;
+            <Arrow />
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className={`m-0 max-w-[30ch] text-sm text-opacity-70`}>
+            Desciption of link. Paragraph a b c d e ipsum ipsum ipsum.
           </p>
         </a>
       </div>
     </main>
-  )
+  );
 }
