d98c4e6bd74649028950ed96c8ace720a2723ea4
Tee Parham <parhameter@gmail.com>
2024-01-30 15:12:29 -0700

    Improve ThemeSwitch

--- app/components/ThemeSwitch.tsx
+++ app/components/ThemeSwitch.tsx
@@ -2,37 +2,9 @@ import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { cx } from "../utils";
 
-interface ToggleButtonProps {
-  css: string; // border
-  newTheme: string;
-  text: string;
-}
-
-function ToggleButton({ css, text, newTheme }: ToggleButtonProps) {
+export function ThemeSwitch() {
   // light, dark, system
   const { theme, setTheme } = useTheme();
-  return (
-    <button
-      className={cx(
-        css,
-        theme === newTheme
-          ? "text-blue-800 bg-gray-100 dark:text-blue-300 dark:bg-gray-600"
-          : "bg-white dark:bg-gray-500",
-        "px-2 py-1 border-gray-400",
-        "hover:bg-gray-200 hover:text-blue-700",
-        "dark:hover:bg-gray-700 dark:hover:text-blue-400",
-        "focus:text-blue-700"
-      )}
-      onClick={() => {
-        setTheme(newTheme);
-      }}
-    >
-      {text}
-    </button>
-  );
-}
-
-export function ThemeSwitch() {
   const [mounted, setMounted] = useState(false);
 
   // only show on client
@@ -45,13 +17,34 @@ export function ThemeSwitch() {
   }
 
   return (
-    <div className="inline-flex text-sm font-medium text-gray-800 dark:text-gray-200">
-      <ToggleButton
-        css="rounded-l-lg border border-r-0"
-        text="Light"
-        newTheme="light"
-      />
-      <ToggleButton css="rounded-r-lg border" text="Dark" newTheme="dark" />
+    <div className="inline-flex text-sm font-medium">
+      <button
+        className={cx(
+          "rounded-l-lg border border-r-0",
+          "px-2 py-1 border-gray-400",
+          "text-gray-800 bg-gray-100",
+          theme === "dark" && "hover:bg-gray-200 hover:text-blue-700"
+        )}
+        onClick={() => {
+          setTheme("light");
+        }}
+      >
+        Light
+      </button>
+
+      <button
+        className={cx(
+          "rounded-r-lg border",
+          "px-2 py-1 border-gray-400",
+          "text-gray-200 bg-gray-700",
+          theme === "light" && "hover:bg-gray-600 hover:text-blue-400"
+        )}
+        onClick={() => {
+          setTheme("dark");
+        }}
+      >
+        Dark
+      </button>
     </div>
   );
 }
