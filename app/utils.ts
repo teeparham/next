04121437169feb56a8dfb83c9c227e3db4435a74
Tee Parham <parhameter@gmail.com>
2024-11-08 15:15:23 -0700

    Add util functions

--- app/utils.ts
+++ app/utils.ts
@@ -5,3 +5,24 @@ export function cx(...args: unknown[]) {
     .join(" ")
     .trim();
 }
+
+// see lodash implementation
+// return true if value is null or undefined
+export function isNil(value: any) {
+  return value == null;
+}
+
+// simpler version of lodash throttle
+export function throttle(callback: Function, limit: number) {
+  var waiting = false;
+  return function () {
+    if (!waiting) {
+      // @ts-ignore this
+      callback.apply(this, arguments);
+      waiting = true;
+      setTimeout(function () {
+        waiting = false;
+      }, limit);
+    }
+  };
+}
