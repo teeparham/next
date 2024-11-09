c010b287f285462076c951843f433471d238d050
Tee Parham <parhameter@gmail.com>
2024-11-09 11:30:57 -0700

    Splash

--- app/2048/Splash.tsx
+++ app/2048/Splash.tsx
@@ -2,12 +2,18 @@ import { useContext } from "react";
 import { GameContext } from "./GameContext";
 import styles from "./styles/splash.module.css";
 import { GameButton } from "../components/GameButton";
+import { cx } from "../utils";
 
 export function Splash({ heading = "You won!", type = "" }) {
   const { startGame } = useContext(GameContext);
 
   return (
-    <div className={`${styles.splash} ${type === "won" && "bg-transparent"}`}>
+    <div
+      className={cx(
+        styles.splash,
+        "bg-gray-400 m-32 rounded-xl border-white border-4"
+      )}
+    >
       <div>
         <h1 className="mb-8 text-gray-100 drop-shadow-2xl">{heading}</h1>
         <GameButton onClick={startGame} text="Play again" />
