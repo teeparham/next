826e957eec38111065cd3a0160235b97bd4ec94e
Tee Parham <parhameter@gmail.com>
2024-11-12 15:49:40 -0700

    Cleanup tile css

--- app/2048/Tile.tsx
+++ app/2048/Tile.tsx
@@ -27,6 +27,21 @@ export type TileType = {
 
 export type TileMap = { [id: string]: TileType };
 
+const bgColorMap = {
+  4: "bg-[#ede0c8]",
+  8: "bg-[#f2b179]",
+  16: "bg-[#f59563]",
+  32: "bg-[#f67c5f]",
+  64: "bg-[#f65e3b]",
+  128: "bg-[#edcf72]",
+  256: "bg-[#edcc61]",
+  512: "bg-[#edc850]",
+  1024: "bg-[#edc53f]",
+  2048: "bg-[#edc22e]",
+  4096: "bg-[#edc22e]",
+  8192: "bg-[#edc22e]",
+};
+
 export function Tile({ position, value }: TileType) {
   const isWideScreen = useMediaQuery({ minWidth: 512 });
   const containerWidth = isWideScreen
@@ -53,15 +68,20 @@ export function Tile({ position, value }: TileType) {
     transform: `scale(${scale})`,
   };
 
+  const fontSize = value > 512 ? "tile-lg" : value > 64 ? "tile-md" : null;
+  // @ts-ignore any type
+  const bgColor = bgColorMap[value];
+  const textColor = value > 4 ? "text-brown-100" : null;
+
   return (
     <div
       className={cx(
-        "bg-brown-200 text-brown-700",
-        "font-bold text-center",
-        "absolute rounded-md",
-        value > 4 && "text-brown-100",
-        styles.tile,
-        styles[`tile${value}`]
+        "bg-brown-200 text-brown-700 m-1 sm:m-2",
+        "font-bold text-center absolute rounded-md",
+        bgColor,
+        textColor,
+        fontSize,
+        styles.tile
       )}
       style={style}
     >
