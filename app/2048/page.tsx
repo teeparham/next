b8011b17f4f82a1958c6ae755e9678b9e5732e47
Tee Parham <parhameter@gmail.com>
2024-11-12 10:49:44 -0700

    Help

--- app/2048/page.tsx
+++ app/2048/page.tsx
@@ -25,11 +25,11 @@ export default function Page() {
         <h1 className="mb-4 text-3xl text-blue-800 dark:text-blue-400">2048</h1>
         <p className="mb-6 text-sm">
           To win, merge tiles with the same numbers to reach the tile with the
-          number 2048. Use the arrow keys to move all tiles in one direction
-          (up, down, left, or right). When two tiles with the same number
-          collide, they combine into one tile with their sum (2 + 2 = 4). Each
-          move adds a new tile (usually a 2 or 4) in a random empty spot. You
-          win if you create a 2048 tile. You lose if no moves are possible.
+          number 2048. Swipe or use the arrow keys to move all tiles in one
+          direction. When two tiles with the same number collide, they combine
+          into one tile with their sum (2 + 2 = 4). Each move adds a new tile
+          (usually a 2 or 4) in a random empty spot. You win if you create a
+          2048 tile. The game is over if no moves are possible.
           <a
             className="mx-2 underline white underline-offset-2 hover:decoration-2"
             href="https://en.wikipedia.org/wiki/2048_(video_game)"
