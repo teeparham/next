aa1536df9d9c8adf40e047610d175726bacef08a
Tee Parham <parhameter@gmail.com>
2024-11-14 12:00:52 -0700

    Rm unused

--- app/2048/constants.ts
+++ app/2048/constants.ts
@@ -1,5 +1,3 @@
-export const containerWidthMobile = 288; // px
-export const containerWidthDesktop = 464; // px
 export const tileCountPerDimension = 4;
 export const mergeAnimationDuration = 100; // ms
 export const gameWinTileValue = 2048;
