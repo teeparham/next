56b3ece0935a308f5a0ce1fc72e9fe03085c704e
Tee Parham <parhameter@gmail.com>
2025-02-12 14:11:56 -0700

    Improve BackHeader

--- app/components/BackHeader.tsx
+++ app/components/BackHeader.tsx
@@ -4,11 +4,10 @@ import { Arrow } from "./Arrow";
 export function BackHeader() {
   return (
     <Link
-      className="block text-xl mb-4 text-neutral-900 dark:text-yellow-400"
+      className="block text-3xl font-bold mb-4 text-neutral-900 dark:text-yellow-400"
       href="/"
     >
       <Arrow left />
-      &nbsp;Tee Parham
     </Link>
   );
 }
